69790c528da2fb70c6aac58e5bb8ee98b9c64f38
Torres9 <Torres9Jin@gmail.com>
2016-07-19 23:06:59 -0700

    remove win support

--- ccutil/platform.h
+++ ccutil/platform.h
@@ -23,20 +23,6 @@
 #include <string.h>
 
 #define DLLSYM
-#ifdef _WIN32
-#ifdef __GNUC__
-#define ultoa _ultoa
-#endif  /* __GNUC__ */
-#define SIGNED
-#if defined(_MSC_VER)
-#if (_MSC_VER < 1900)
-#define snprintf _snprintf
-#endif
-#if (_MSC_VER <= 1400)
-#define vsnprintf _vsnprintf
-#endif /* (_MSC_VER <= 1400) */
-#endif /* defined(_MSC_VER) */
-#else
 #define __UNIX__
 #include <limits.h>
 #ifndef PATH_MAX
@@ -45,44 +31,11 @@
 #define MAX_PATH PATH_MAX
 #endif
 #define SIGNED signed
-#endif
 
-#if defined(_WIN32) || defined(__CYGWIN__)
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
-#endif
+#define TESS_API  __attribute__ ((visibility ("default")))
+#define TESS_LOCAL  __attribute__ ((visibility ("hidden")))
 
-#if defined(_WIN32) || defined(__CYGWIN__)
-    #if defined(TESS_EXPORTS)
-       #define TESS_API __declspec(dllexport)
-    #elif defined(TESS_IMPORTS)
-       #define TESS_API __declspec(dllimport)
-    #else
-       #define TESS_API
-    #endif
-    #define TESS_LOCAL
-#else
-    #if __GNUC__ >= 4
-      #if defined(TESS_EXPORTS) || defined(TESS_IMPORTS)
-          #define TESS_API  __attribute__ ((visibility ("default")))
-          #define TESS_LOCAL  __attribute__ ((visibility ("hidden")))
-      #else
-          #define TESS_API
-          #define TESS_LOCAL
-      #endif
-    #else
-      #define TESS_API
-      #define TESS_LOCAL
-    #endif
-#endif
-
-#if defined(_WIN32) || defined(__CYGWIN__)
-    #define _TESS_FILE_BASENAME_                                            \
-      (strrchr(__FILE__, '\\') ? strrchr(__FILE__, '\\') + 1 : __FILE__)
-#else   // Unices
-    #define _TESS_FILE_BASENAME_                                            \
-      (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
-#endif
+#define _TESS_FILE_BASENAME_ \
+(strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 
 #endif  // TESSERACT_CCUTIL_PLATFORM_H__
