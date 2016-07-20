d1e81d5946db739e2fac29c5ba5d8394d0170f6b
Torres9 <Torres9Jin@gmail.com>
2016-07-19 18:40:53 -0700

    remove win support

--- ccutil/ccutil.h
+++ ccutil/ccutil.h
@@ -26,12 +26,8 @@
 #include "params.h"
 #include "unicharset.h"
 
-#ifdef _WIN32
-#include <windows.h>
-#else
 #include <pthread.h>
 #include <semaphore.h>
-#endif
 
 namespace tesseract {
 
@@ -43,11 +39,7 @@ class CCUtilMutex {
 
   void Unlock();
  private:
-#ifdef _WIN32
-  HANDLE mutex_;
-#else
   pthread_mutex_t mutex_;
-#endif
 };
 
 
@@ -82,10 +74,6 @@ class CCUtil {
   // These have to be declared and initialized after params_ member, since
   // params_ should be initialized before parameters are added to it.
   STRING_VAR_H(m_data_sub_dir, "tessdata/", "Directory for data files");
-  #ifdef _WIN32
-  STRING_VAR_H(tessedit_module_name, WINDLLNAME,
-               "Module colocated with tessdata dir");
-  #endif
   INT_VAR_H(ambigs_debug_level, 0, "Debug level for unichar ambiguities");
   BOOL_VAR_H(use_definite_ambigs_for_classifier, 0,
              "Use definite ambiguities when running character classifier");
